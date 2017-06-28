smarkets/marge-bot

commit 26093d60b6fe284be7872413ed6464a1b0a4fcf2

Add Reviewed-by: commit message headers on sufficient approval.
- Adds some logic to pull out the reviewers for a commit and add headers for
  them to each commit introduced by PR.

- Also, add Tested-by: marge-bot <$PR_URL> header on the last commit. This is
  both useful for bisection (to skip commits without this header) as to see
  where something actually came from in a rebase-based workflow.

- Adds some re-approval logic on marge pushing

- Warns if marge is not an admin (with work around for gitlab bug)

marge/bot.py
@@ -3,12 +3,14 @@ import time
 from datetime import datetime, timedelta
 from tempfile import TemporaryDirectory
 
+from .approvals import Approvals
+
 from . import commit as commit_module
 from . import git
 from . import gitlab
 from . import merge_request as merge_request_module
 from . import project as project_module
-
+from .user import User
 
 Commit = commit_module.Commit
 MergeRequest = merge_request_module.MergeRequest
@@ -16,9 +18,34 @@ Project = project_module.Project
 
 
 class Bot(object):
-    def __init__(self, *, api, user, project, ssh_key_file=None):
+    def __init__(
+            self,
+            *,
+            api,
+            user,
+            project,
+            ssh_key_file,
+            add_reviewers=True,
+            add_tested=True,
+            impersonate_approvers=True
+    ):
         assert project.merge_requests_enabled
         assert project.only_allow_merge_if_build_succeeds
+        # There's a bug in some recent versions of Gitlab, where is_admin is
+        # not set, even for admins. Use sudo (which only admins can do) as a
+        # hack as work around.
+        # See e.g. <https://gitlab.com/gitlab-org/gitlab-ce/issues/34325>
+        if user.is_admin is None:
+            try:
+                user.myself(api=api, sudo=user.id)
+                user._info['is_admin'] = True  # pylint: disable=protected-access
+            except gitlab.Forbidden:
+                pass
+        if not user.is_admin:
+            assert not impersonate_approvers, "{0.username} is not an admin, can't impersonate!".format(user)
+            assert not add_reviewers, (
+                "{0.username} is not an admin, can't lookup Reviewed-by: email addresses ".format(user)
+            )
 
         self._ssh_key_file = ssh_key_file
         self.max_ci_waiting_time = timedelta(minutes=15)
@@ -28,6 +55,10 @@ class Bot(object):
         self._api = api
         self._project = project
         self._user = user
+
+        self._add_reviewers = add_reviewers
+        self._add_tested = add_tested
+        self._impersonate_approvers = impersonate_approvers
 
     def start(self):
         while True:
@@ -52,13 +83,20 @@ class Bot(object):
     def _run(self, repo):
         while True:
             log.info('Fetching merge requests assigned to me...')
-            all_merge_requests = MergeRequest.fetch_all_opened(self._project.id, self._api)
-            my_merge_requests = [mr for mr in all_merge_requests if mr.assignee_id == self._user.id]
-
+            my_merge_requests = MergeRequest.fetch_all_open_for_user(
+                project_id=self._project.id,
+                user_id=self._user.id,
+                api=self._api
+            )
             log.info('Got %s requests to merge', len(my_merge_requests))
             for merge_request in my_merge_requests:
                 merge_request.refetch_info()
-                self.process_merge_request(merge_request, repo)
+                approvals = Approvals.fetch_approvals_for_merge_request(
+                    self._project.id,
+                    merge_request.id,
+                    self._api,
+                )
+                self.process_merge_request(merge_request, repo, approvals)
 
             time_to_sleep_in_secs = 60
             log.info('Sleeping for %s seconds...', time_to_sleep_in_secs)
@@ -68,7 +106,7 @@ class Bot(object):
         now = datetime.utcnow()
         return any(interval.covers(now) for interval in self.embargo_intervals)
 
-    def process_merge_request(self, merge_request, repo):
+    def process_merge_request(self, merge_request, repo, approvals):
         log.info('Processing !%s - %r', merge_request.iid, merge_request.title)
 
         if self._user.id != merge_request.assignee_id:
@@ -86,18 +124,11 @@ class Bot(object):
             return
 
         try:
-            project_id = merge_request.project_id
-            source_project_id = merge_request.source_project_id
-            target_project_id = merge_request.target_project_id
-
-            if not project_id == source_project_id == target_project_id:
-                raise CannotMerge("I don't yet know how to handle merge requests from different projects")
-
             if self.during_merge_embargo():
                 log.info('Merge embargo! -- SKIPPING')
                 return
 
-            self.rebase_and_accept_merge_request(merge_request, repo)
+            self.rebase_and_accept_merge_request(merge_request, repo, approvals)
             log.info('Successfully merged !%s.', merge_request.info['iid'])
         except CannotMerge as err:
             message = "I couldn't merge this branch: %s" % err.reason
@@ -120,37 +151,77 @@ class Bot(object):
         else:
             mr.unassign()
 
-    def rebase_and_accept_merge_request(self, merge_request, repo):
-        previous_sha = merge_request.sha
-        no_failure = object()
-        last_failure = no_failure
-        merged = False
-
-        while not merged:
+    def rebase_and_accept_merge_request(self, merge_request, repo, approvals):
+        rebased_into_up_to_date_target_branch = False
+        while not rebased_into_up_to_date_target_branch:
             if merge_request.work_in_progress:
                 raise CannotMerge("Sorry, I can't merge requests marked as Work-In-Progress!")
-
-            # NB. this will be a no-op if there is nothing to rebase
-            actual_sha = push_rebased_version(
-                repo, merge_request.source_branch, merge_request.target_branch
+            approvals.refetch_info()
+            if not approvals.sufficient:
+                raise CannotMerge(
+                    'Insufficient approvals '
+                    '(have: {0.approver_usernames} missing: {0.approvals_left})'.format(approvals)
+                )
+            tested_by = (
+                ['{0._user.name} <{1.web_url}>'.format(self, merge_request)] if self._add_tested
+                else None
             )
-            if last_failure is not no_failure:
-                if actual_sha == previous_sha:
-                    raise CannotMerge('merge request was rejected by GitLab: %r' % last_failure)
-
+            reviewers = (
+                _get_reviewer_names_and_emails(approvals=approvals, api=self._api) if self._add_reviewers
+                else None
+            )
+            source_project = (
+                self._project if merge_request.source_project_id == self._project.id else
+                Project.fetch_by_id(merge_request.source_project_id, api=self._api)
+            )
+            source_repo_url = None if source_project is self._project else source_project.ssh_url_to_repo
+            # NB. this will be a no-op if there is nothing to rebase/rewrite
+            target_sha, _rebased_sha, actual_sha = push_rebased_and_rewritten_version(
+                repo=repo,
+                source_branch=merge_request.source_branch,
+                target_branch=merge_request.target_branch,
+                source_repo_url=source_repo_url,
+                reviewers=reviewers,
+                tested_by=tested_by,
+            )
             log.info('Commit id to merge %r', actual_sha)
             time.sleep(5)
 
-            self.wait_for_ci_to_pass(actual_sha)
+            self.wait_for_ci_to_pass(source_project.id, actual_sha)
             log.info('CI passed!')
             time.sleep(2)
 
+            sha_now = Commit.last_on_branch(source_project.id, merge_request.source_branch, self._api).id
+            # Make sure no-one managed to race and push to the branch in the
+            # meantime, because we're about to impersonate the approvers, and
+            # we don't want to approve unreviewed commits
+            if sha_now != actual_sha:
+                raise CannotMerge('Someone pushed to branch while we were trying to merge')
+            # Re-approve the merge request, in case us pushing it has removed
+            # approvals. Note that there is a bit of a race; effectively
+            # approval can't be withdrawn after we've pushed (resetting
+            # approvals) and CI runs.
+            if self._impersonate_approvers:
+                approvals.reapprove()
             try:
                 merge_request.accept(remove_branch=True, sha=actual_sha)
             except gitlab.NotAcceptable as err:
-                log.info('Not acceptable! -- %s', err.error_message)
-                last_failure = err.error_message
-                previous_sha = actual_sha
+                new_target_sha = Commit.last_on_branch(
+                    self._project.id,
+                    merge_request.target_branch,
+                    self._api
+                ).id
+                # target_branch has moved under us since we rebased, just try again
+                if new_target_sha != target_sha:
+                    merge_request.comment(
+                        "My job would be easier if people didn't jump the queue and pushed directly... *sigh*"
+                    )
+                    continue
+                # otherwise the source branch has been pushed to or something
+                # unexpected went wrong in either case, we expect the user to
+                # explicitly re-assign to marge (after resolving potential
+                # problems)
+                raise CannotMerge('Merge request was rejected by GitLab: %r' % err.error_message)
             except gitlab.Unauthorized:
                 log.warning('Unauthorized!')
                 raise CannotMerge('My user cannot accept merge requests!')
@@ -159,13 +230,7 @@ class Bot(object):
                 raise CannotMerge('had some issue with gitlab, check my logs...')
             else:
                 self.wait_for_branch_to_be_merged(merge_request)
-                merged = True
-
-        if last_failure is not no_failure:
-            merge_request.comment(
-                "My job would be easier if people didn't jump the queue and pushed directly... *sigh*"
-            )
-
+                rebased_into_up_to_date_target_branch = True
 
     def wait_for_branch_to_be_merged(self, merge_request):
         time_0 = datetime.utcnow()
@@ -185,12 +250,12 @@ class Bot(object):
 
         raise CannotMerge('It is taking too long to see the request marked as merged!')
 
-    def wait_for_ci_to_pass(self, commit_sha):
+    def wait_for_ci_to_pass(self, project_id, commit_sha):
         time_0 = datetime.utcnow()
         waiting_time_in_secs = 10
 
         while datetime.utcnow() - time_0 < self.max_ci_waiting_time:
-            ci_status = Commit.fetch_by_id(self._project.id, commit_sha, self._api).status
+            ci_status = Commit.fetch_by_id(project_id, commit_sha, self._api).status
             if ci_status == 'success':
                 return
 
@@ -219,32 +284,92 @@ class CannotMerge(Exception):
         return args[0]
 
 
-def push_rebased_version(repo, source_branch, target_branch):
-    if source_branch == target_branch:
+def push_rebased_and_rewritten_version(
+        repo,
+        source_branch,
+        target_branch,
+        source_repo_url=None,
+        reviewers=None,
+        tested_by=None,
+):
+    """Rebase `target_branch` into `source_branch`, optionally add trailers and push.
+
+    Parameters
+    ----------
+    source_branch
+       The branch we want to rebase to.
+    target_branch
+       The branch we want to rebase from.
+    source_repo_url
+       The url of the repo we want to push the changes to (or `None` if it's the
+       same repo for both `source_branch` and `target_branch`).
+    reviewers
+       A list, possibly empty, if we should add reviewer information or `None`
+       if we should not add reviewer information. The difference between
+       ``None`` and ``[]``  (or another list) is that with ``None`` we leave existing
+       Reviewed-by: trailers in place  whereas with a list argument we replace them.
+    tested_by
+       A list like ``["User Name <u.name@invalid.com>", ...]`` or `None`. ``None`` means
+       existing Tested-by lines will be left alone, otherwise they will be replaced.
+
+    Returns
+    -------
+    (sha_of_target_branch, sha_after_rebase, sha_after_rewrite)
+    """
+    assert source_repo_url != repo.remote_url
+    if source_repo_url is None and source_branch == target_branch:
         raise CannotMerge('source and target branch seem to coincide!')
 
-    branch_rebased, changes_pushed = False, False
-    sha = None
+    branch_rebased = branch_rewritten = changes_pushed = False
+    rebased_sha = rewritten_sha = None
     try:
-        repo.rebase(branch=source_branch, new_base=target_branch)
+        rebased_sha = repo.rebase(
+            branch=source_branch,
+            new_base=target_branch,
+            source_repo_url=source_repo_url
+        )
         branch_rebased = True
-
-        sha = repo.get_head_commit_hash()
-
-        repo.push_force(source_branch)
+        if reviewers is not None:
+            rewritten_sha = repo.tag_with_trailer(
+                trailer_name='Reviewed-by',
+                trailer_values=reviewers,
+                branch=source_branch,
+                start_commit=['source/', 'origin/'][source_repo_url is None] + target_branch,
+            )
+        if tested_by is not None:
+            rewritten_sha = repo.tag_with_trailer(
+                trailer_name='Tested-by',
+                trailer_values=tested_by,
+                branch=source_branch,
+                start_commit=source_branch+'^'
+            )
+        branch_rewritten = True
+        repo.push_force(source_branch, source_repo_url)
         changes_pushed = True
     except git.GitError:
         if not branch_rebased:
             raise CannotMerge('got conflicts while rebasing, your problem now...')
-
+        if not branch_rewritten:
+            raise CannotMerge('failed on filter-branch; check my logs!')
         if not changes_pushed:
             raise CannotMerge('failed to push rebased changes, check my logs!')
 
         raise
     else:
-        return sha
+        target_sha = repo.get_commit_hash(target_branch)
+        return target_sha, rebased_sha, rewritten_sha
     finally:
         # A failure to clean up probably means something is fucked with the git repo
         # and likely explains any previous failure, so it will better to just
         # raise a GitError
-        repo.remove_branch(source_branch)
+        if source_branch != 'master':
+            repo.remove_branch(source_branch)
+        else:
+            assert source_repo_url is not None
+
+
+def _get_reviewer_names_and_emails(approvals, api):
+    """Return a list ['A. Prover <a.prover@example.com', ...]` for `merge_request.`"""
+
+    uids = approvals.approver_ids
+    return ['{0.name} <{0.email}>'.format(User.fetch_by_id(uid, api)) for uid in uids]

default.nix
@@ -9,12 +9,12 @@ python.mkDerivation {
   src = ./.;
   # The dependencies, referring to variables in <nixpkgs>.
   buildInputs = [py.pylint py.pytest py.pytest-cov];
-  propagatedBuildInputs = [py.maya py.requests pkgs.openssh pkgs.perl pkgs.git];
+  propagatedBuildInputs = [py.maya py.requests pkgs.openssh pkgs.git];
   checkPhase = ''
      export NO_TESTS_OVER_WIRE=1
      export PYTHONDONTWRITEBYTECODE=1
      #export PYTHONPATH=$PYTHONPATH:/.
-     pylint marge
+     pylint marge || [[ $? = 8 ]]  # ignore refactoring comments for exit code
      # FIXME(alexander): why do I need to screw w/ PYTHONPATH?
      PYTHONPATH=$PYTHONPATH:. py.test --cov marge
   '';

marge/approvals.py
@@ -1,0 +1,49 @@
+from . import gitlab
+
+GET, POST, PUT = gitlab.GET, gitlab.POST, gitlab.PUT
+
+
+class Approvals(gitlab.Resource):
+    """Approval info for a MergeRequest."""
+
+    @classmethod
+    def fetch_approvals_for_merge_request(cls, project_id, merge_request_id, api):
+        approvals = cls(api, {'id': merge_request_id, 'project_id': project_id})
+        approvals.refetch_info()
+        return approvals
+
+    def refetch_info(self):
+        approver_url = '/projects/{0.project_id}/merge_requests/{0.id}/approvals'.format(self)
+        self._info = self._api.call(GET(approver_url))
+
+    @property
+    def project_id(self):
+        return self.info['project_id']
+
+    @property
+    def approvals_left(self):
+        return self.info['approvals_left'] or 0
+
+    @property
+    def sufficient(self):
+        return not self.info['approvals_left']
+
+    @property
+    def approver_usernames(self):
+        return [who['user']['username'] for who in self.info['approved_by']]
+
+    @property
+    def approver_ids(self):
+        """Return the uids of the approvers."""
+        return [who['user']['id'] for who in self.info['approved_by']]
+
+    def reapprove(self):
+        """Impersonates the approvers and re-approves the merge_request as them.
+
+        The idea is that we want to get the approvers, push the rebased branch
+        (which may invalidate approvals, depending on gitlab settings) and then
+        restore the approval status.
+        """
+        approve_url = '/projects/{0.project_id}/merge_requests/{0.id}/approve'.format(self)
+        for uid in self.approver_ids:
+            self._api.call(POST(approve_url), sudo=uid)

marge/commit.py
@@ -1,3 +1,5 @@
+import re
+
 from . import gitlab
 
 
@@ -9,8 +11,21 @@ class Commit(gitlab.Resource):
     @classmethod
     def fetch_by_id(cls, project_id, sha, api):
         info = api.call(GET(
-            '/projects/%s/repository/commits/%s' % (project_id, sha),
+            '/projects/{project_id}/repository/commits/{sha}'.format(
+                project_id=project_id,
+                sha=sha,
+            ),
         ))
+        return cls(api, info)
+
+    @classmethod
+    def last_on_branch(cls, project_id, branch, api):
+        info = api.call(GET(
+            '/projects/{project_id}/repository/branches/{branch}'.format(
+                project_id=project_id,
+                branch=branch,
+            ),
+        ))['commit']
         return cls(api, info)
 
     @property
@@ -32,3 +47,11 @@ class Commit(gitlab.Resource):
     @property
     def status(self):
         return self.info['status']
+
+    @property
+    def reviewers(self):
+        return re.findall(r'^Reviewed-by: ([^\n]+)$', self.info['message'], re.MULTILINE)
+
+    @property
+    def testers(self):
+        return re.findall(r'^Tested-by: ([^\n]+)$', self.info['message'], re.MULTILINE)

marge/git.py
@@ -6,7 +6,23 @@ from subprocess import PIPE, TimeoutExpired
 
 from collections import namedtuple
 
+
+from . import trailerfilter
+
 TIMEOUT_IN_SECS = 60
+
+
+def _filter_branch_script(trailer_name, trailer_values):
+    filter_script = 'TRAILERS={trailers} {script}'.format(
+        trailers=shlex.quote(
+            '\n'.join(
+                '{}: {}'.format(trailer_name, trailer_value)
+                for trailer_value in trailer_values or [''])
+        ),
+        script=trailerfilter.__file__,
+    )
+    return filter_script
+
 
 class Repo(namedtuple('Repo', 'remote_url local_path ssh_key_file')):
     def clone(self):
@@ -16,28 +32,62 @@ class Repo(namedtuple('Repo', 'remote_url local_path ssh_key_file')):
         self.git('config', 'user.email', user_email)
         self.git('config', 'user.name', user_name)
 
-    def rebase(self, branch, new_base):
-        assert branch != new_base, branch
+    def tag_with_trailer(self, trailer_name, trailer_values, branch, start_commit):
+        """Replace `trailer_name` in commit messages with `trailer_values` in `branch` from `start_commit`.
+        """
 
-        success = False
+        # Strips all `$trailer_name``: lines and trailing newlines, adds an empty
+        # newline and tags on the `$trailer_name: $trailer_value` for each `trailer_value` in
+        # `trailer_values`.
+        filter_script = _filter_branch_script(trailer_name, trailer_values)
+        commit_range = start_commit + '..' + branch
+        try:
+            # --force = overwrite backup of last filter-branch
+            self.git('filter-branch', '--force', '--msg-filter', filter_script, commit_range)
+        except GitError:
+            log.warning('filter-branch failed, will try to restore')
+            self.git('reset', '--hard', 'refs/original/refs/heads/' + branch)
+            raise
+        return self.get_commit_hash()
+
+    def rebase(self, branch, new_base, source_repo_url=None):
+        """Rebase `new_base` into `branch` and return the new HEAD commit id.
+
+        By default `branch` and `new_base` are assumed to reside in the same
+        repo as `self`. However, if `source_repo_url` is passed and not `None`,
+        `branch` is taken from there.
+
+        Throws a `GitError` if the rebase fails. Will also try to --abort it.
+        """
+        assert source_repo_url or branch != new_base, branch
 
         self.git('fetch', 'origin')
-        self.git('checkout', branch, '--')
+        if source_repo_url:
+            # "upsert" remote 'source' and fetch it
+            try:
+                self.git('remote', 'rm', 'source')
+            except GitError:
+                pass
+            self.git('remote', 'add', 'source', source_repo_url)
+            self.git('fetch', 'source')
+            self.git('checkout', '-B', branch, 'source/' + branch, '--')
+        else:
+            self.git('checkout', '-B', branch, 'origin/' + branch, '--')
 
         try:
             self.git('rebase', 'origin/%s' % new_base)
-            success = True
         except GitError:
+            log.warning('rebase failed, doing an --abort')
             self.git('rebase', '--abort')
-
-        return success
+            raise
+        return self.get_commit_hash()
 
     def remove_branch(self, branch):
         assert branch != 'master'
         self.git('checkout', 'master', '--')
         self.git('branch', '-D', branch)
 
-    def push_force(self, branch):
+    def push_force(self, branch, source_repo_url=None):
         self.git('checkout', branch, '--')
 
         self.git('diff-index', '--quiet', 'HEAD')  # check it is not dirty
@@ -46,11 +96,20 @@ class Repo(namedtuple('Repo', 'remote_url local_path ssh_key_file')):
         if untracked_files:
             raise GitError('There are untracked files', untracked_files)
 
-        self.git('push', '--force', 'origin', branch)
+        if source_repo_url:
+            assert self.get_remote_url('source') == source_repo_url
+            source = 'source'
+        else:
+            source = 'origin'
+        self.git('push', '--force', source, branch)
 
-    def get_head_commit_hash(self):
-        result = self.git('rev-parse', 'HEAD')
-        return str(result.stdout, encoding='ascii').strip()
+    def get_commit_hash(self, rev='HEAD'):
+        """Return commit hash for `rev` (default "HEAD")."""
+        result = self.git('rev-parse', rev)
+        return result.stdout.decode('ascii').strip()
+
+    def get_remote_url(self, name):
+        return self.git('config', '--get', 'remote.{}.url'.format(name)).stdout.decode('utf-8').strip()
 
     def git(self, *args, from_repo=True):
         env = None
@@ -93,6 +152,7 @@ def _run(*args, env=None, check=False, timeout=None):
                 retcode, process.args, output=stdout, stderr=stderr,
             )
         return subprocess.CompletedProcess(process.args, retcode, stdout, stderr)
+
 
 class GitError(Exception):
     pass

marge/gitlab.py
@@ -9,10 +9,12 @@ class Api(object):
         self._auth_token = auth_token
         self._api_base_url = gitlab_url.rstrip('/') + '/api/v3'
 
-    def call(self, command):
+    def call(self, command, sudo=None):
         method = command.method
         url = self._api_base_url + command.endpoint
         headers = {'PRIVATE-TOKEN': self._auth_token}
+        if sudo:
+            headers['SUDO'] = '%d' % sudo
         response = method(url, headers=headers, **command.call_args)
 
         if response.status_code == 200:

marge/merge_request.py
@@ -13,17 +13,21 @@ class MergeRequest(gitlab.Resource):
         return merge_request
 
     @classmethod
-    def fetch_all_opened(cls, project_id, api):
-        merge_requests = api.collect_all_pages(GET(
-            '/projects/%s/merge_requests' % project_id,
+    def fetch_all_open_for_user(cls, project_id, user_id, api):
+        all_merge_request_infos = api.collect_all_pages(GET(
+            '/projects/{project_id}/merge_requests'.format(project_id=project_id),
             {'state': 'opened', 'order_by': 'created_at', 'sort': 'asc'},
         ))
+        my_merge_request_infos = [
+            mri for mri in all_merge_request_infos
+            if (mri['assignee'] or {}).get('id') == user_id
+        ]
 
-        return [cls(api, merge_request_info) for merge_request_info in merge_requests]
+        return [cls(api, merge_request_info) for merge_request_info in my_merge_request_infos]
 
     @property
     def project_id(self):
-        return self._info['project_id']
+        return self.info['project_id']
 
     @property
     def iid(self):
@@ -69,6 +73,14 @@ class MergeRequest(gitlab.Resource):
     @property
     def work_in_progress(self):
         return self.info['work_in_progress']
+
+    @property
+    def approved_by(self):
+        return self.info['approved_by']
+
+    @property
+    def web_url(self):
+        return self.info['web_url']
 
     def refetch_info(self):
         self._info = self._api.call(GET('/projects/%s/merge_requests/%s' % (self.project_id, self.id)))

marge/project.py
@@ -38,3 +38,7 @@ class Project(gitlab.Resource):
     @property
     def only_allow_merge_if_build_succeeds(self):
         return self.info['only_allow_merge_if_build_succeeds']
+
+    @property
+    def approvals_required(self):
+        return self.info['approvals_before_merge']

commit ff04f836ecf8f09ca5b52d2361e4fee26bb4fe93

BUGFIX: Crash when ran without rewritting commits
Introduced by 26093d60.

marge/bot.py
@@ -321,9 +321,8 @@ def push_rebased_and_rewritten_version(
         raise CannotMerge('source and target branch seem to coincide!')
 
     branch_rebased = branch_rewritten = changes_pushed = False
-    rebased_sha = rewritten_sha = None
     try:
-        rebased_sha = repo.rebase(
+        rewritten_sha = rebased_sha = repo.rebase(
             branch=source_branch,
             new_base=target_branch,
             source_repo_url=source_repo_url