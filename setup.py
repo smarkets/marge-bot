188e4c6b5385828225fd4f7b1978f6b41ab66767
Alexander Schmolck <alexander.schmolck@smarkets.com>
2017-07-05 00:23:52 +0100

    Read software version from `version`

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+import os
 from distutils.core import setup
+VERSION = open(os.path.join(os.path.dirname(__file__),  'version')).read().strip()
 setup(
     name='marge',
-    version='0.1',
+    version=VERSION,
     license='BSD3',
     packages=['marge'],
     scripts=['marge.app'],
