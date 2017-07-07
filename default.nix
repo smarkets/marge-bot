1d33c29da25ab8aff313416b290461a881c6e32f
Alexander Schmolck <alexander.schmolck@smarkets.com>
2017-07-07 11:45:05 +0100

    Pin nixpkgs and use a version that fixes git bug
    Nix's git filter-branch is broken due to missing deps on grep and sed, the
    forked version I've pinned to fixes that (it's based off 1b57bf274a, which at
    the time of this commit is the current channel revision).

--- default.nix
+++ default.nix
@@ -1,3 +1,3 @@
-let pkgs = import <nixpkgs> {};
+let pkgs = import ./pinnedNixpkgs.nix;
 in
 pkgs.callPackage ./marge.nix {}
---
+++ pinnedNixpkgs.nix
@@ -0,0 +1,1 @@
+import (fetchTarball "https://github.com/smarkets/nixpkgs/archive/344bb95eb899698bb3d4a94d41fde55db4dfbbf2.tar.gz") { }
