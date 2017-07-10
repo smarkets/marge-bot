7b66bb8c82041bbf34067af0ecc0306510980c8a
Alexander Schmolck <alexander.schmolck@smarkets.com>
2017-07-10 17:50:28 +0100

    pinnedNixpkgs: bump to latest fix

--- pinnedNixpkgs.nix
+++ pinnedNixpkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/smarkets/nixpkgs/archive/344bb95eb899698bb3d4a94d41fde55db4dfbbf2.tar.gz") { }
+import (fetchTarball "https://github.com/smarkets/nixpkgs/archive/b2f82bbadc8157a7f68d2be62b9341a400d827a3.tar.gz") { }
