f40b365d0de44c7d941060b447968070bb3ea3ab
Marian Rusu <rusumarian91@gmail.com>
2017-11-28 15:54:37 +0000

    use pkgs explicitly
    Makes it clear what's going on
    
    Signed-off-by: Marian Rusu <rusumarian91@gmail.com>

--- dockerize.nix
+++ dockerize.nix
@@ -1,16 +1,15 @@
 { pkgs ? import ./pinnedNixpkgs.nix }:
-with pkgs;
 let callPackage = pkgs.lib.callPackageWith (pkgs);
-  marge = callPackage ./marge.nix {};
-  version = marge.version;
+    marge = callPackage ./marge.nix {};
+    version = marge.version;
 in
-dockerTools.buildImage {
+pkgs.dockerTools.buildImage {
   name = "smarkets/marge-bot";
   tag = "${version}";
   # minimal user setup, so ssh won't whine 'No user exists for uid 0'
   runAsRoot = ''
-  #!${stdenv.shell}
-  ${dockerTools.shadowSetup}
+  #!${pkgs.stdenv.shell}
+  ${pkgs.dockerTools.shadowSetup}
   mkdir -p /root/.ssh
   '';
   contents = [marge pkgs.bash pkgs.coreutils pkgs.openssh pkgs.glibcLocales];
