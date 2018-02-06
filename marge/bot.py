b330e98eefd004b978c7eced6c8506c5d081f328
Marian Rusu <rusumarian91@gmail.com>
2018-02-06 14:58:30 +0000

    Refactor Bot._run
    Makes it arguably cleaner and passing lint
    
    Signed-off-by: Marian Rusu <rusumarian91@gmail.com>

--- marge/bot.py
+++ marge/bot.py
@@ -50,70 +50,91 @@ class Bot(object):
         time_to_sleep_between_projects_in_secs = 1
         min_time_to_sleep_after_iterating_all_projects_in_secs = 30
         while True:
-            log.info('Finding out my current projects...')
-            my_projects = Project.fetch_all_mine(self._api)
-            project_regexp = self._config.project_regexp
-            filtered_projects = [p for p in my_projects if project_regexp.match(p.path_with_namespace)]
-            filtered_out = set(my_projects) - set(filtered_projects)
-            if filtered_out:
-                log.debug(
-                    'Projects that match project_regexp: %s',
-                    [p.path_with_namespace for p in filtered_projects]
-                )
-                log.debug(
-                    'Projects that do not match project_regexp: %s',
-                    [p.path_with_namespace for p in filtered_out]
-                )
-            for project in filtered_projects:
-                project_name = project.path_with_namespace
-
-                if project.access_level.value < AccessLevel.reporter.value:
-                    log.warning("Don't have enough permissions to browse merge requests in %s!", project_name)
-                    continue
-
-                log.info('Fetching merge requests assigned to me in %s...', project_name)
-                my_merge_requests = MergeRequest.fetch_all_open_for_user(
-                    project_id=project.id,
-                    user_id=self.user.id,
-                    api=self._api
-                )
-                branch_regexp = self._config.branch_regexp
-                filtered_mrs = [mr for mr in my_merge_requests
-                                if branch_regexp.match(mr.target_branch)]
-                filtered_out = set(my_merge_requests) - set(filtered_mrs)
-                if filtered_out:
-                    log.debug(
-                        'MRs that match branch_regexp: %s',
-                        [mr.web_url for mr in filtered_mrs]
-                    )
-                    log.debug(
-                        'MRs that do not match branch_regexp: %s',
-                        [mr.web_url for mr in filtered_out]
-                    )
-
-                if filtered_mrs:
-                    log.info('Got %s requests to merge; will try to merge the oldest', len(filtered_mrs))
-                    merge_request = filtered_mrs[0]
-                    try:
-                        repo = repo_manager.repo_for_project(project)
-                    except git.GitError:
-                        log.exception("Couldn't initialize repository for project!")
-                        raise
-
-                    merge_job = job.MergeJob(
-                        api=self._api, user=self.user,
-                        project=project, merge_request=merge_request, repo=repo,
-                        options=self._config.merge_opts,
-                    )
-                    merge_job.execute()
-                else:
-                    log.info('Nothing to merge at this point...')
-                time.sleep(time_to_sleep_between_projects_in_secs)
+            projects = self._get_projects()
+            self._process_projects(
+                repo_manager,
+                time_to_sleep_between_projects_in_secs,
+                projects,
+            )
             big_sleep = max(0,
                             min_time_to_sleep_after_iterating_all_projects_in_secs -
-                            time_to_sleep_between_projects_in_secs * len(filtered_projects))
+                            time_to_sleep_between_projects_in_secs * len(projects))
             log.info('Sleeping for %s seconds...', big_sleep)
             time.sleep(big_sleep)
+
+    def _get_projects(self):
+        log.info('Finding out my current projects...')
+        my_projects = Project.fetch_all_mine(self._api)
+        project_regexp = self._config.project_regexp
+        filtered_projects = [p for p in my_projects if project_regexp.match(p.path_with_namespace)]
+        log.debug(
+            'Projects that match project_regexp: %s',
+            [p.path_with_namespace for p in filtered_projects]
+        )
+        filtered_out = set(my_projects) - set(filtered_projects)
+        if filtered_out:
+            log.debug(
+                'Projects that do not match project_regexp: %s',
+                [p.path_with_namespace for p in filtered_out]
+            )
+        return filtered_projects
+
+    def _process_projects(
+        self,
+        repo_manager,
+        time_to_sleep_between_projects_in_secs,
+        projects,
+    ):
+        for project in projects:
+            project_name = project.path_with_namespace
+
+            if project.access_level.value < AccessLevel.reporter.value:
+                log.warning("Don't have enough permissions to browse merge requests in %s!", project_name)
+                continue
+            merge_requests = self._get_merge_requests(project, project_name)
+            self._process_merge_requests(repo_manager, project, merge_requests)
+            time.sleep(time_to_sleep_between_projects_in_secs)
+
+    def _get_merge_requests(self, project, project_name):
+        log.info('Fetching merge requests assigned to me in %s...', project_name)
+        my_merge_requests = MergeRequest.fetch_all_open_for_user(
+            project_id=project.id,
+            user_id=self.user.id,
+            api=self._api
+        )
+        branch_regexp = self._config.branch_regexp
+        filtered_mrs = [mr for mr in my_merge_requests
+                        if branch_regexp.match(mr.target_branch)]
+        log.debug(
+            'MRs that match branch_regexp: %s',
+            [mr.web_url for mr in filtered_mrs]
+        )
+        filtered_out = set(my_merge_requests) - set(filtered_mrs)
+        if filtered_out:
+            log.debug(
+                'MRs that do not match branch_regexp: %s',
+                [mr.web_url for mr in filtered_out]
+            )
+        return filtered_mrs
+
+    def _process_merge_requests(self, repo_manager, project, merge_requests):
+        if merge_requests:
+            log.info('Got %s requests to merge; will try to merge the oldest', len(merge_requests))
+            merge_request = merge_requests[0]
+            try:
+                repo = repo_manager.repo_for_project(project)
+            except git.GitError:
+                log.exception("Couldn't initialize repository for project!")
+                raise
+
+            merge_job = job.MergeJob(
+                api=self._api, user=self.user,
+                project=project, merge_request=merge_request, repo=repo,
+                options=self._config.merge_opts,
+            )
+            merge_job.execute()
+        else:
+            log.info('Nothing to merge at this point...')
 
 
 class BotConfig(namedtuple('BotConfig',
