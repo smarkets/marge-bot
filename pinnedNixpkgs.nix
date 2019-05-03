abdb5d16d218389fca50da6625f628036d76872a
Oleh Stolyar <oleh.stolyar@smarkets.com>
2019-05-03 14:41:26 +0100

    nix: formatting, smarter `src` in marge.nix

--- pinnedNixpkgs.nix
+++ pinnedNixpkgs.nix
@@ -1,9 +1,10 @@
 let
   fetchFromGitHub = (import <nixpkgs> {}).fetchFromGitHub;
-  pkgs = import (fetchFromGitHub {
-                   owner  = "NixOS";
-                   repo   = "nixpkgs";
-                   rev    = "90afb0c10fe6f437fca498298747b2bcb6a77d39";
-                   sha256 = "0mvzdw5aygi1vjnvm0bc8bp7iwb9rypiqg749m6a6km84m7srm0w";
-                 }) {};
-in pkgs
+  pinned = fetchFromGitHub {
+    owner  = "NixOS";
+    repo   = "nixpkgs";
+    rev    = "90afb0c10fe6f437fca498298747b2bcb6a77d39";
+    sha256 = "0mvzdw5aygi1vjnvm0bc8bp7iwb9rypiqg749m6a6km84m7srm0w";
+  };
+in
+  import pinned {}
