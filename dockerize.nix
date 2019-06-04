smarkets/marge-bot

commit 95d31c72b5a5255389e88965eefe2bbbb6e8d1fe

More nix build tweaks (auto-updateable nixpkgs; no runAsRoot etc)
- Bump nixpkgs
- Jsonify specs
- make it auto-updateable (`make bump-nixpkgs`)
- streamline nix files a bit more
- dockerize.nix: get rid of nasty runAsRoot (runs qumu, making it not possible
  to work in isolated build etc).
- shell.nix to inject dev tools into nix-shell

dockerize.nix
@@ -2,18 +2,22 @@
 let
   marge = pkgs.callPackage ./marge.nix {};
   version = marge.version;
+  basicShadow =
+    # minimal user setup, so ssh won't whine 'No user exists for uid 0'
+    pkgs.runCommand "basic-shadow-setup" {}
+      ''
+        mkdir -p $out
+        cd $out
+        ${pkgs.dockerTools.shadowSetup}
+        mkdir -p root/.ssh
+      '';
 in
   pkgs.dockerTools.buildImage {
     name = "smarkets/marge-bot";
     tag = "${version}";
-    # minimal user setup, so ssh won't whine 'No user exists for uid 0'
-    runAsRoot = ''
-      #!${pkgs.stdenv.shell}
-      ${pkgs.dockerTools.shadowSetup}
-      mkdir -p /root/.ssh
-    '';
     contents =
       with pkgs; [
+        basicShadow
         bash
         coreutils
         git
@@ -22,9 +26,6 @@ in
       ] ++ [ marge ];
     config = {
       Entrypoint = [ "/bin/marge.app" ];
-      Env = [
-        "LANG=en_US.UTF-8"
-        "LOCALE_ARCHIVE=/lib/locale/locale-archive"
-      ];
+      Env = ["LANG=en_US.UTF-8" ''LOCALE_ARCHIVE=/lib/locale/locale-archive''];
     };
   }

Makefile
@@ -1,37 +1,46 @@
 VERSION?=$$(git rev-parse --abbrev-ref HEAD)
 
-requirements_frozen.txt requirements.nix requirements_override.nix: requirements.txt
-	pypi2nix -V 3.6 -r $^
-
 .PHONY: all
-all: requirements_frozen.txt requirements.nix requirements_override.nix default.nix
-	nix-build -K .
+all: requirements_frozen.txt requirements.nix requirements_override.nix marge-bot dockerize
+
+.PHONY: marge-bot
+marge-bot:
+	nix-build --keep-failed --attr marge-bot default.nix
 
 .PHONY: clean
 clean:
 	rm -rf .cache result requirements_frozen.txt
 
+.PHONY: bump
+bump: bump-requirements bump-sources
+
+.PHONY: bump-sources
+bump-sources:
+	nix-shell --run niv update
+
 .PHONY: bump-requirements
 bump-requirements: clean requirements_frozen.txt
 
-.PHONY: dockerize
-dockerize: dockerize.nix
-	docker load --input $$(nix-build dockerize.nix)
+requirements_frozen.txt requirements.nix requirements_override.nix: requirements.txt
+	pypi2nix -V 3.6 -r $^
 
+.PHONY: dockerize
+dockerize:
+	docker load --input $$(nix-build --attr docker-image default.nix)
 
 .PHONY: docker-push
 docker-push:
 	if [ -n "$$DOCKER_USERNAME" -a -n "$$DOCKER_PASSWORD" ]; then \
-	  docker login -u "$${DOCKER_USERNAME}" -p "$${DOCKER_PASSWORD}"; \
+		docker login -u "$${DOCKER_USERNAME}" -p "$${DOCKER_PASSWORD}"; \
 	else \
-	  docker login; \
+		docker login; \
 	fi
 	docker tag smarkets/marge-bot:$$(cat version) smarkets/marge-bot:$(VERSION)
 	if [ "$(VERSION)" = "$$(cat version)" ]; then \
-	  docker tag smarkets/marge-bot:$$(cat version) smarkets/marge-bot:latest; \
-	  docker tag smarkets/marge-bot:$$(cat version) smarkets/marge-bot:stable; \
-	  docker push smarkets/marge-bot:stable; \
-	  docker push smarkets/marge-bot:latest; \
+		docker tag smarkets/marge-bot:$$(cat version) smarkets/marge-bot:latest; \
+		docker tag smarkets/marge-bot:$$(cat version) smarkets/marge-bot:stable; \
+		docker push smarkets/marge-bot:stable; \
+		docker push smarkets/marge-bot:latest; \
 	fi
 	docker push smarkets/marge-bot:$(VERSION)
 	# for backwards compatibility push to previous location

default.nix
@@ -1,4 +1,6 @@
-let
-  pkgs = import ./pinnedNixpkgs.nix;
-in
-  pkgs.callPackage ./marge.nix {}
+let sources = import ./nix/sources.nix; in
+with import sources.nixpkgs {};
+{
+  marge-bot = callPackage ./marge.nix {};
+  docker-image = callPackage ./dockerize.nix {};
+}

nix/sources.nix
@@ -1,0 +1,6 @@
+# Read in the json spec for packages we want (so it can be auto-updated).
+# niv: no_update
+with builtins;
+mapAttrs
+  (_: spec: spec // { outPath = fetchTarball { inherit (spec) url sha256; }; })
+  (fromJSON (readFile ./sources.json))

pinnedNixpkgs.nix
@@ -1,10 +1,8 @@
 let
-  fetchFromGitHub = (import <nixpkgs> {}).fetchFromGitHub;
-  pinned = fetchFromGitHub {
-    owner  = "NixOS";
-    repo   = "nixpkgs";
-    rev    = "90afb0c10fe6f437fca498298747b2bcb6a77d39";
-    sha256 = "0mvzdw5aygi1vjnvm0bc8bp7iwb9rypiqg749m6a6km84m7srm0w";
+  spec = builtins.fromJSON (builtins.readFile ./pinnedNixpkgs.src.json);
+  src = builtins.fetchTarball {
+    url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
+    sha256 = spec.sha256;
   };
 in
-  import pinned {}
+  import src {}

shell.nix
@@ -1,0 +1,14 @@
+let
+  addBuildTools = pkg: tools: pkg.overrideAttrs
+     (oldAttrs: { nativeBuildInputs = oldAttrs.nativeBuildInputs ++ tools; });
+  sources = import ./nix/sources.nix;
+  ## Tool to bump versions of sources written as json entries to git repos etc.
+  ## We use it bump nixpkgs itself ATM (just `niv update`).
+  niv = (import sources.niv {}).niv;
+  pkgs = (import sources.nixpkgs {});
+  pypi2nix = pkgs.pypi2nix;
+  make = pkgs.make;
+  marge-bot = (import ./.).marge-bot;
+in
+  ## create a version of the marge-bot env that has niv
+  addBuildTools marge-bot [ niv pypi2nix ]

commit 2121e0908287c298ad1057d8038c83b92fb679d2

Fix docker image root user creation broken in 95d31c7

dockerize.nix
@@ -8,8 +8,18 @@ let
       ''
         mkdir -p $out
         cd $out
-        ${pkgs.dockerTools.shadowSetup}
         mkdir -p root/.ssh
+        mkdir -p etc/pam.d
+        echo "root:x:0:0::/root:/bin/sh" >etc/passwd
+        echo "root:!x:::::::" > etc/shadow
+        echo "root:x:0:" > etc/group
+        echo "root:x::" > etc/gshadow
+        cat > etc/pam.d/other <<EOF
+        account sufficient pam_unix.so
+        auth sufficient pam_rootok.so
+        password requisite pam_unix.so nullok sha512
+        session required pam_unix.so
+        EOF
       '';
 in
   pkgs.dockerTools.buildImage {