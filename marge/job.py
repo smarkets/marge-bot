33fb81b77005bbce0047cf2cf7faccc4e5728660
Geoffroy <geoffroy.carlotti@zhinst.com>
2022-04-07 11:11:26 +0200

    Ignore jobs with manual trigger in CI

--- marge/job.py
+++ marge/job.py
@@ -182,6 +182,10 @@ class MergeJob:
             if ci_status == 'skipped':
                 log.info('CI for MR !%s skipped', merge_request.iid)
                 return
+            
+            if ci_status == 'manual':
+                log.info('CI for MR !%s has manual jobs', merge_request.iid)
+                return
 
             if ci_status == 'failed':
                 raise CannotMerge('CI failed!')
