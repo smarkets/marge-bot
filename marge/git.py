smarkets/marge-bot

commit fc60c1c17de413fdb949bb8e4c82140268b3616f

Use '--rebase-merges' when rebasing
Avoids eliding merge commits on MR source branch.

marge/git.py
@@ -99,7 +99,7 @@ class Repo(namedtuple('Repo', 'remote_url local_path ssh_key_file timeout refere
 
         Throws a `GitError` if the rebase fails. Will also try to --abort it.
         """
-        return self._fuse_branch('rebase', branch, new_base, source_repo_url=source_repo_url, local=local)
+        return self._fuse_branch('rebase', branch, new_base, '--rebase-merges', source_repo_url=source_repo_url, local=local)
 
     def _fuse_branch(self, strategy, branch, target_branch, *fuse_args, source_repo_url=None, local=False):
         assert source_repo_url or branch != target_branch, branch

commit bdd9c6a30ea6448d77895a82b140bafccf6e4803

Split arguments lines of rebase call
fixes `line too long` lint failure
it's introduced by fc60c1c

marge/git.py
@@ -99,7 +99,8 @@ class Repo(namedtuple('Repo', 'remote_url local_path ssh_key_file timeout refere
 
         Throws a `GitError` if the rebase fails. Will also try to --abort it.
         """
-        return self._fuse_branch('rebase', branch, new_base, '--rebase-merges', source_repo_url=source_repo_url, local=local)
+        return self._fuse_branch('rebase', branch, new_base, '--rebase-merges',
+                                 source_repo_url=source_repo_url, local=local)
 
     def _fuse_branch(self, strategy, branch, target_branch, *fuse_args, source_repo_url=None, local=False):
         assert source_repo_url or branch != target_branch, branch